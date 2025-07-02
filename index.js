58ad07c6417139f720c7215166cef7768717bd53
Amanite <brunaudsamuel@gmail.com>
2025-07-02 13:58:51 +0200

    v2 (ajout erreur)

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var c = document.getElementById("myCanvas");
 c.setAttribute('style', 'display: none');
 var ctx = c.getContext("2d");
-/*ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();*/
+
 
 const height = document.getElementById("height");
 const width = document.getElementById("width");
 const myButton = document.getElementById("Creer");
 
 function MyMap() {
-    if (width.value == NaN || height.value == NaN || width.value <= 0 || height.value <= 0) {
-        return;
+    if (isNaN(width.value) || isNaN(height.value) || width.value <= 0 || height.value <= 0 || width.value > 1000 || height.value > 450) {
+      alert("Erreur : veuillez rentrer des dimensions correctes.");  
+      return;
     }
     
     c.setAttribute('width', width.value);
